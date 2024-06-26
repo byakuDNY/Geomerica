208f83685bee25cbc303c1ded73b699b10f3fc42
byakuDNY <r13luo@gmail.com>
2024-06-26 14:41:12 -0500

    p

--- server/models/country.model.js
+++ server/models/country.model.js
@@ -1,28 +1,31 @@
 import mongoose from "mongoose";
 
-const countrySchema = new mongoose.Schema({
-  pais: {
-    type: String,
-    required: true,
+const countrySchema = new mongoose.Schema(
+  {
+    pais: {
+      type: String,
+      required: true,
+    },
+    capital: {
+      type: String,
+      required: true,
+    },
+    continente: {
+      type: String,
+      required: true,
+    },
+    bandera: {
+      type: String,
+    },
+    mapa: {
+      type: String,
+    },
+    localizacion: {
+      type: String,
+    },
   },
-  capital: {
-    type: String,
-    required: true,
-  },
-  continente: {
-    type: String,
-    required: true,
-  },
-  bandera: {
-    type: String,
-  },
-  mapa: {
-    type: String,
-  },
-  localizacion: {
-    type: String,
-  },
-});
+  { collection: "Paises" }
+);
 
 const Country = mongoose.model("Paises", countrySchema);
 
--- server/routes/country.routes.js
+++ server/routes/country.routes.js
@@ -7,21 +7,27 @@ import {
   getFiveBandera,
   getCountry,
   deleteCountry,
+  getFiveLocalizacion,
+  getFiveMapa,
 } from "../controllers/country.controller.js";
 
 const router = express.Router();
 
 router.get("/", getCountries);
 
-router.post("/", createCountry);
-
-router.get("/five-capitals", getFiveCapitals);
-
-router.get("/five-countries-name", getFiveCountriesName);
-
-router.get("/five-bandera", getFiveBandera);
-
 router.get("/:id", getCountry);
+
+router.get("/cinco-capitals", getFiveCapitals);
+
+router.get("/cinco-nombre-paises", getFiveCountriesName);
+
+router.get("/cinco-banderas", getFiveBandera);
+
+router.get("/cinco-mapas", getFiveMapa);
+
+router.get("/cinco-localizacion", getFiveLocalizacion);
+
+router.post("/", createCountry);
 
 router.delete("/:id", deleteCountry);
 
