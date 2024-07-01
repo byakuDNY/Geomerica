9c74e9dae7abf72593a48f0ef17785e6e2bbc2e6
Roberto Luo <r13luo.gmail.com>
2024-07-01 00:18:55 -0500

    cors issues

--- server/server.js
+++ server/server.js
@@ -11,7 +11,11 @@ const PORT = process.env.PORT || 5000;
 
 dotenv.config();
 
-app.use(cors());
+const corsOptions = {
+  origin: "https://worldwise-eir.pages.dev/",
+};
+app.use(cors(corsOptions));
+
 app.use(express.json()); // para analizar las solicitudes entrantes con cargas json (from req.body)
 app.use(express.urlencoded({ extended: false }));
 
