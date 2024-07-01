20e6f4090c5d4afe556ed20f1de9e634d2700e71
Roberto Luo <r13luo.gmail.com>
2024-07-01 14:30:57 -0500

    vercel.json and server.js changes

--- server/server.js
+++ server/server.js
@@ -10,7 +10,13 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
