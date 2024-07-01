6ad3da0355972830e6ac7b62b62be8b230befd2c
Roberto Luo <r13luo.gmail.com>
2024-07-01 00:59:38 -0500

    cors

--- server/server.js
+++ server/server.js
@@ -1,26 +1,33 @@
 import express from "express";
 import dotenv from "dotenv";
-
 import cors from "cors";
 import authRoutes from "./routes/auth.routes.js";
 import countryRoutes from "./routes/country.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
+dotenv.config();
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-dotenv.config();
-
 const corsOptions = {
-  origin: "https://worldwise-eir.pages.dev",
+  origin: "https://worldwise-eir.pages.dev", // Removed trailing slash
+  optionsSuccessStatus: 200, // Some legacy browsers choke on 204
 };
-app.use(cors(corsOptions));
 
-app.use(express.json()); // para analizar las solicitudes entrantes con cargas json (from req.body)
+app.use(cors(corsOptions));
+app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/country", countryRoutes);
+
+app.use((req, res, next) => {
+  console.log(`Request URL: ${req.url}`);
+  console.log(`Request Method: ${req.method}`);
+  console.log(`Request Headers: ${JSON.stringify(req.headers)}`);
+  next();
+});
 
 app.get("/", (req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "*"); // Ensure headers are set before sending the response
