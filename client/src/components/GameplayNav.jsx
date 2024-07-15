0c10e79f38a5efd46a60b644fbe822685ce170a9
Roberto Luo <r13luo.gmail.com>
2024-07-15 11:11:37 -0500

    update_project commit to marge

---
+++ client/src/components/GameplayNav.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import questionMark from "../assets/questionMark.png";
+import { Link } from "react-router-dom";
+const GameplayNav = () => {
+  let { name } = useParams();
+
+  return (
+    <nav className="flex justify-between bg-[#2C4058] p-3">
+      <div className="flex items-center gap-5 text-[#F3F3F3] px-4 py-2 text-2xl font-semibold ">
+        <img src="/question-mark.png" alt="question-mark-logo" />
+        {name}
+      </div>
+      <Link
+        to={"/"}
+        className="flex justify-center w-10 h-10 font-extrabold text-3xl text-white rounded-full hover:bg-[#252B41] hover:text-red-500"
+      >
+        &times;
+      </Link>
+    </nav>
+  );
+};
+
+export default GameplayNav;
--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -4,26 +4,30 @@ export const VITE_BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 
 const Homepage = () => {
   const modes = [
-    { id: "1", name: "Modo 1 : Adivina el Capital" },
-    { id: "2", name: "Modo 2 : Adivina la Bandera" },
-    { id: "3", name: "Modo 3 : Adivina el País según el Mapa del País" },
-    { id: "4", name: "Modo 4 : Adivina el País según el Mapa del Región" },
+    { id: "1", name: "Modo 1 : Adivina la Bandera" },
+    { id: "2", name: "Modo 2 : Quiz de Geolocalización" },
+    { id: "3", name: "Modo 3 : Adivina la Cuidad Capital" },
   ];
 
   return (
-    <div className="flex flex-col justify-center items-center gap-10 w-screen h-screen bg-gradient-to-b from-[#2c69da] via-[#4edaf9] to-[#2c69da] ">
-      <h1 className="text-[#F3F3F3] font-normal text-4xl stroke-blue-700 stroke-2">
-        WorldWise
-      </h1>
-      {modes.map((mode) => (
-        <Link
-          key={mode.id}
-          to={`/gameplay/${mode.id}/${mode.name}`}
-          className="bg-transparent text-[#F3F3F3] border-2 rounded-lg px-4 py-2 text-2xl font-medium hover:bg-[#F3F3F3] hover:text-[#2c69da] hover:cursor-pointer w-2/5"
-        >
-          {mode.name}
-        </Link>
-      ))}
+    <div>
+      <nav className="flex justify-center items-center bg-[#2C4058] p-8 rounded-b-3xl header-curved">
+        <h1 className="font-extralight text-[#F3F3F3] text-6xl stroke-blue-700 stroke-2">
+          AmeriQuest
+        </h1>
+      </nav>
+      <div className="flex flex-col justify-center items-center gap-10 pt-16">
+        {modes.map((mode) => (
+          <Link
+            key={mode.id}
+            to={`/gameplay/${mode.id}/${mode.name}`}
+            className="flex items-center gap-5 bg-[#2C4058] text-[#F3F3F3] rounded-lg px-4 py-2 text-2xl font-medium hover:bg-[#ffd15b] hover:text-[#252B41] hover:cursor-pointer w-2/5"
+          >
+            <img src="/question-mark.png" alt="question-mark-logo" />
+            {mode.name}
+          </Link>
+        ))}
+      </div>
     </div>
   );
 };
